77933498eb493e2417966d1bcb76f19e2fb7eb91
elkin.torres <elkin.torres@groupcos.com.co>
2024-09-17 23:09:10 -0500

    Props and emits

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,13 +1,41 @@
 <template>
   <div class="post-card">
-    <h3>Tittle</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nam exercitationem aspernatur maxime velit voluptatum voluptates non inventore numquam, aliquid veritatis obcaecati, iure, quod dignissimos libero quae perspiciatis neque? Voluptas.</p>
+    <h3>{{ props.title }}</h3>
+    <p>{{ props.content }} {{ props.size }}</p>
+    <button @click="handleClick">Say Hi</button>
     <PostButton />
   </div>
 </template>
 
-<script lang="ts" setup>
-import PostButton from './PostButton.vue';
+<script lang="ts">
+// import PostButton from './PostButton.vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'PostCard',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      default: 'No tiene contenido ***',
+    },
+    size: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+  emits: ['sayHi'],
+  setup(props, { emit }) {
+    const handleClick = () => {
+      emit('sayHi', 'Hiiiiiiiiiiiiii');
+    };
+    return { props, handleClick};
+  },
+});
 </script>
 
 <style scoped>
--- src/components/PostContainer.vue
+++ src/components/PostContainer.vue
@@ -1,11 +1,44 @@
 <template>
   <div class="post-container">
-    <PostCard />
+    <PostCard v-for="(info, i) in infos" :key="i" :title="info.title" :content="info.content" :size="info.size" @sayHi="showAlert" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import PostCard from './PostCard.vue';
+
+interface IInfo {
+  title: string;
+  content: string;
+  size: number;
+}
+
+let infos: Array<IInfo> = [
+  {
+    title: 'Papitas',
+    content: 'Hola como tas',
+    size: 415,
+  },
+  {
+    title: 'Yucas',
+    content: 'como tas',
+    size: 786,
+  },
+  {
+    title: 'Pailas',
+    content: 'Hola como tas',
+    size: 584,
+  },
+  {
+    title: 'Papitas',
+    content: 'Hola como tas',
+    size: 415,
+  },
+];
+
+function showAlert(msg: string) {
+  alert(msg);
+}
 </script>
 
 <style scoped>
